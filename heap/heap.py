299713cbdf29d1d34ce9bab1470cd2d42acd9498
Moin Uddin <U_Moin75@yahoo.com>
2019-02-25 18:45:37 -0500

    Implementing Heap

--- heap/heap.py
+++ heap/heap.py
@@ -1,21 +1,42 @@
+from math import floor
 class Heap:
   def __init__(self):
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    if len(self.storage) > 1:
+      self._bubble_up(len(self.storage) - 1)
+    print(self.storage)
 
   def delete(self):
-    pass
+    root = self.storage.pop(0)
+    return root
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    child = self.storage[index]
+    parent = self.storage[floor((index - 1) / 2)]
+    #If parent is less than child, swap
+    if parent < child:
+      self.storage[floor((index - 1) / 2)], self.storage[index] = self.storage[index], self.storage[floor((index - 1) / 2)]
 
   def _sift_down(self, index):
-    pass
+    p = (index-1)/2
+    print(p)
+
+
+heap = Heap()
+heap.insert(6)
+heap.insert(8)
+heap.insert(10)
+heap.insert(9)
+heap.insert(1)
+heap.insert(9)
+heap.insert(9)
+heap.insert(5)
