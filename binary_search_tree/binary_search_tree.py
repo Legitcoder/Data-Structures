631f64b0bae804901d9059068d840ab56f94c859
Moin Uddin <U_Moin75@yahoo.com>
2019-02-26 03:10:59 -0500

    Implement BinarySearchTree methods insert and contains

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,15 +5,42 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    if self is None: print("It's None")
-    while self.value is not None:
-      if self.value < value:
-        pass
-      elif self.value > value:
-        pass
+    node = self
+    #Traverse through Tree
+    while node.value is not None:
+      #If smaller than root traverse left
+      if node.value > value:
+        if node.left is None:
+          node.left = BinarySearchTree(value)
+          node = node.left
+          break
+        node = node.left
+      #If larger than root traverse right
+      if node.value < value:
+        if node.right is None:
+          node.right = BinarySearchTree(value)
+          node = node.right
+          break
+        node = node.right
+
 
   def contains(self, target):
-    pass
+    node = self
+    if node.value == target: return True
+    while node is not None:
+      if node.value > target:
+        node = node.left
+        if node is None:
+          return False
+          break
+        if node.value == target: return True
+      if node.value < target:
+        node = node.right
+        if node is None:
+          return False
+          break
+        if node.value == target: return True
+
 
   def get_max(self):
     pass
@@ -21,7 +48,10 @@ class BinarySearchTree:
 bst = BinarySearchTree(5)
 bst.insert(2)
 bst.insert(3)
+print(bst.value)
 bst.insert(7)
 bst.insert(6)
-print(bst.left.right.value)
+print(bst.left.value)
+print(bst.right.value)
 print(bst.right.left.value)
+print(bst.left.right.value)
